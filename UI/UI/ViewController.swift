a6be51f61cfc021cd4a0c2b662c483c9850ae415
DimMalysh <d.malysh@mail.ru>
2018-03-22 20:33:18 +0200

    UI with Autolayout and SizeClasses
    Custom UI with Autolayout and SizeClasses

---
+++ UI/UI/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  UI
+//
+//  Created by mac on 22.03.18.
+//  Copyright © 2018 Dim Malysh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
